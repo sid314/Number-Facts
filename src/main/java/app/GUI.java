27abc2c80f28a0dcc3025c093f16ee2a49f36bbe
sid314 <81903825+sid314@users.noreply.github.com>
2022-02-02 11:33:37 +0530

    Made code prettier

--- src/main/java/app/GUI.java
+++ src/main/java/app/GUI.java
@@ -1,71 +1,82 @@
 package app;
-import javax.swing.*;
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class GUI extends JFrame implements ActionListener{
-    JFrame frame;
-    JButton button;
-    JPanel panel;
-    JLabel inputLabel;
-    JTextField inputtf;
-    JLabel outputLabel;
-    JTextArea output;
-    JButton reset;
-    String res = "";
-    public  GUI(){
-        //frame
-        frame = new JFrame("Number Properties");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        //button
-        button = new JButton("Submit");
-        reset = new JButton("Reset");
-        //panel
-        panel = new JPanel(); 
-        //label
-        inputLabel = new JLabel("Enter a number");
-        
-        //text field
-        inputtf = new JTextField(10); // characters accepted
-        
-        //text area
-        output = new JTextArea();
-        output.setEditable(false);
-       // Components Added using Flow Layout
-       
-        panel.add(inputLabel); 
-        panel.add(inputtf);
-        panel.add(button);
-        panel.add(output);
-        panel.add(reset);
-        //allignment
-        frame.getContentPane().add(BorderLayout.CENTER, panel);
-        
-        frame.setVisible(true);
-        button.addActionListener(this);
-        reset.addActionListener(this);
+public class GUI extends JFrame implements ActionListener {
+
+  JFrame frame;
+  JButton button;
+  JPanel panel;
+  JLabel inputLabel;
+  JTextField inputtf;
+  JLabel outputLabel;
+  JTextArea output;
+  JButton reset;
+  String res = "";
+
+  public GUI() {
+    //frame
+    frame = new JFrame("Number Properties");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 300);
+    //button
+    button = new JButton("Submit");
+    reset = new JButton("Reset");
+    //panel
+    panel = new JPanel();
+    //label
+    inputLabel = new JLabel("Enter a number");
+
+    //text field
+    inputtf = new JTextField(10); // characters accepted
+
+    //text area
+    output = new JTextArea();
+    output.setEditable(false);
+    // Components Added using Flow Layout
+
+    panel.add(inputLabel);
+    panel.add(inputtf);
+    panel.add(button);
+    panel.add(output);
+    panel.add(reset);
+    //allignment
+    frame.getContentPane().add(BorderLayout.CENTER, panel);
+
+    frame.setVisible(true);
+    button.addActionListener(this);
+    reset.addActionListener(this);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    res = "";
+    if (e.getSource() == button) {
+      int n = Integer.parseInt(inputtf.getText());
+      res =
+        res +
+        mainClass.prime(n) +
+        mainClass.SumOfEvenDigits(n) +
+        mainClass.SumOfDigits(n) +
+        mainClass.SumOfOddDigits(n) +
+        mainClass.Automorphic(n) +
+        mainClass.Palindrome(n) +
+        mainClass.EvenDigit(n) +
+        mainClass.OddDigit(n) +
+        mainClass.SpyNumber(n) +
+        mainClass.Niven(n) +
+        mainClass.Neon(n) +
+        mainClass.Buzz(n) +
+        mainClass.pronic(n) +
+        mainClass.Special(n);
+      output.setText(res);
+    } else if (e.getSource() == reset) {
+      res = "";
+      output.setText(null);
+      inputtf.setText(null);
     }
-    
-    @Override
-        public void actionPerformed(ActionEvent e) {
-           res = "";
-            if(e.getSource()==button){
-                 int n=Integer.parseInt(inputtf.getText());
-                 res = res + mainClass.prime(n)+ mainClass.SumOfEvenDigits(n)+
-                 mainClass.SumOfDigits(n)+mainClass.SumOfOddDigits(n)+mainClass.Automorphic(n)+
-                 mainClass.Palindrome(n)+mainClass.EvenDigit(n)+mainClass.OddDigit(n)+mainClass.SpyNumber(n)+
-                 mainClass.Niven(n)+mainClass.Neon(n)+mainClass.Buzz(n)+
-                 mainClass.pronic(n)+mainClass.Special(n);
-                 output.setText(res);
-            }
-            else if(e.getSource()==reset){
-             
-             res = "";
-             output.setText(null);
-             inputtf.setText(null);
-         }
-            
-     }
+  }
 }
--- src/main/java/app/app.java
+++ src/main/java/app/app.java
@@ -1,142 +1,156 @@
 package app;
+
 class mainClass {
- public static void main(String[] args){
-    GUI main = new GUI();  
-    main.dispose(); 
-   }
-   public static String prime(int n){
-        int m=n,c=0,i=1;
-        do{
-            if(m%i==0)
-            c++;
-            i++;
-        }while(i<=m);
-        return (c==2?"Prime":"Not Prime")+"\n";
+
+  public static void main(String[] args) {
+    GUI main = new GUI();
+    main.dispose();
+  }
+
+  public static String prime(int n) {
+    int m = n, c = 0, i = 1;
+    do {
+      if (m % i == 0) c++;
+      i++;
+    } while (i <= m);
+    return (c == 2 ? "Prime" : "Not Prime") + "\n";
+  }
+
+  public static String Automorphic(int n) {
+    int sq = n * n, k = 0, m = n;
+    while (m != 0) {
+      k++;
+      m /= 10;
     }
-    public static String Automorphic(int n){
-        int sq= n*n, k = 0, m=n;
-           while(m!=0){
-               k++;
-               m/=10;
-           }
-           int r = sq%(int)Math.pow(10,k);
-           return (r==n?"Automorphic":"Not Automorphic")+"\n" ;
+    int r = sq % (int) Math.pow(10, k);
+    return (r == n ? "Automorphic" : "Not Automorphic") + "\n";
+  }
+
+  public static String Palindrome(int n) {
+    int m = n, r = 0;
+    while (m != 0) {
+      int d = m % 10;
+      r = r * 10 + d;
+      m /= 10;
     }
-    public static String Palindrome(int n){
-        int m= n,r=0;
-        while(m!=0){
-            int d= m%10;
-            r= r*10+d;
-            m/=10;
-        }
-        return (r==n?"Palindrome":"Not palindrome")+"\n";
-    }
-    public static String EvenDigit(int n){
-        int m =n, c= 0;
-        while(m!=0){
-            int d = m%10;
-            if(d%2==0){
-                c++;
-               }
-            m/=10;
-        }
-         return "Number of even digits = "+ c+"\n";
-    }
-    public static String OddDigit(int n){
-        int m =n, c= 0;
-        while(m!=0){
-            int d = m%10;
-            if(d%2==1){
-                c++;    
-            }
-            m/=10;
-        }
-        return "No of Odd digits ="+ c+"\n";
-    }
-    public static String SumOfEvenDigits(int n){
-        int m =n, s= 0;
-        while(m!=0){
-            int d = m%10;
-            if(d%2==0)
-            s=s+d;
-            m/=10;
-        }
-         return "Sum of Even digits = "+ s+"\n";
-    }
-    public static String SumOfOddDigits(int n){
-        int m =n, s= 0;
-        while(m!=0){
-            int d = m%10;
-            if(d%2==1)
-            s=s+d;
-            m/=10;
-        }
-        return "Sum of odd digits ="+ s+"\n";
-        }
-    public static String SumOfDigits(int n){
-        int m =n, s= 0;
-        while(m!=0){
-            int d = m%10;
-            
-            s=s+d;
-            m/=10;
-        }
-         return "Sum of digits = "+ s+"\n";
-        }
-    public static String SpyNumber(int n){
-      int m=n, s= 0, p=1;
-      while(m!=0){
-          int d = m%10;
-          s+=d;
-          p*=d;
-          m/=10;
+    return (r == n ? "Palindrome" : "Not palindrome") + "\n";
+  }
+
+  public static String EvenDigit(int n) {
+    int m = n, c = 0;
+    while (m != 0) {
+      int d = m % 10;
+      if (d % 2 == 0) {
+        c++;
       }
-     return (s==p?"Spy Number":"Not Spy Number")+"\n";
+      m /= 10;
     }
-    public static String Niven(int n){
-    int m = n, s= 0;
-    while (m!=0) {
-        int d = m%10;
-        s+=d;
-        m/=10;
-        }
-    return (n%s==0?"Niven number":"Not Niven Number")+"\n";
+    return "Number of even digits = " + c + "\n";
+  }
+
+  public static String OddDigit(int n) {
+    int m = n, c = 0;
+    while (m != 0) {
+      int d = m % 10;
+      if (d % 2 == 1) {
+        c++;
+      }
+      m /= 10;
     }
-    public static String Neon(int n){
-        int m = n*n,s=0;
-        while (m!=0) {
-            int d = m%10;
-            s +=d;
-            m/=10;
-        }
-        return (s==n?"Neon number":"Not Neon Number")+"\n";
+    return "No of Odd digits =" + c + "\n";
+  }
+
+  public static String SumOfEvenDigits(int n) {
+    int m = n, s = 0;
+    while (m != 0) {
+      int d = m % 10;
+      if (d % 2 == 0) s = s + d;
+      m /= 10;
     }
-    public static String Buzz (int n){
-       return ((n%7==0)||(n%10==7)?"Buzz Number":"Not A Buzz Number")+"\n";
+    return "Sum of Even digits = " + s + "\n";
+  }
+
+  public static String SumOfOddDigits(int n) {
+    int m = n, s = 0;
+    while (m != 0) {
+      int d = m % 10;
+      if (d % 2 == 1) s = s + d;
+      m /= 10;
     }
-    public static String pronic(int n){
-        int f = 0;
-        for (int i =1;i<=n;i++){
-         if(i*(i+1)==n){
-             f=1;
-             break;
-         }
-        }
-        return (f==1?"Pronic":"Not Pronic")+"\n";
+    return "Sum of odd digits =" + s + "\n";
+  }
+
+  public static String SumOfDigits(int n) {
+    int m = n, s = 0;
+    while (m != 0) {
+      int d = m % 10;
+
+      s = s + d;
+      m /= 10;
     }
-    public static String Special(int n){
-        int m=n,s = 0;
-       while (m!=0) {
-           int d = m%10;
-           int f=1;
-           for (int i = 1; i<=d;i++) {
-               f*=i;
-           }
-           s+=f;
-           m/=10;
-       }
-       return (s==n?"Special":"Not Special")+"\n";
-     }
- }
-   
-    
+    return "Sum of digits = " + s + "\n";
+  }
+
+  public static String SpyNumber(int n) {
+    int m = n, s = 0, p = 1;
+    while (m != 0) {
+      int d = m % 10;
+      s += d;
+      p *= d;
+      m /= 10;
+    }
+    return (s == p ? "Spy Number" : "Not Spy Number") + "\n";
+  }
+
+  public static String Niven(int n) {
+    int m = n, s = 0;
+    while (m != 0) {
+      int d = m % 10;
+      s += d;
+      m /= 10;
+    }
+    return (n % s == 0 ? "Niven number" : "Not Niven Number") + "\n";
+  }
+
+  public static String Neon(int n) {
+    int m = n * n, s = 0;
+    while (m != 0) {
+      int d = m % 10;
+      s += d;
+      m /= 10;
+    }
+    return (s == n ? "Neon number" : "Not Neon Number") + "\n";
+  }
+
+  public static String Buzz(int n) {
+    return (
+      ((n % 7 == 0) || (n % 10 == 7) ? "Buzz Number" : "Not A Buzz Number") +
+      "\n"
+    );
+  }
+
+  public static String pronic(int n) {
+    int f = 0;
+    for (int i = 1; i <= n; i++) {
+      if (i * (i + 1) == n) {
+        f = 1;
+        break;
+      }
+    }
+    return (f == 1 ? "Pronic" : "Not Pronic") + "\n";
+  }
+
+  public static String Special(int n) {
+    int m = n, s = 0;
+    while (m != 0) {
+      int d = m % 10;
+      int f = 1;
+      for (int i = 1; i <= d; i++) {
+        f *= i;
+      }
+      s += f;
+      m /= 10;
+    }
+    return (s == n ? "Special" : "Not Special") + "\n";
+  }
+}
